78371dd1978407836c66174eeee8179f69124d58
Jacob <jacob.zhou.ca@gmail.com>
2018-02-26 19:19:40 +0000

    Base code course 1

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+	<head></head>
+	<body>
+		
+	 <?php
+	 $x=0;
+
+	 if (empty($x)) {
+	 	echo "empty is working for 0";
+	 }
+
+	 	// if statement
+
+	 	$weather = 3;
+
+	 	if ($weather == "good") {
+	 		echo "Let's play basketball";
+	 	} else{
+	 		echo "Let's play videogame";
+	 	}
+		// $city = "ottawa";
+
+		// $city = "toronto";
+
+		// $x = 24 % 5;
+
+		// // echo $x;
+
+		// echo "This is php part 1<br>" . $x;
+
+		// 	echo $city;
+		// 	echo "php part 2"
+
+		?>
+		<br>
+		html 2 
+	</body>	
+</html>
---
+++ test.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php //if statement
+phpinfo();
+ $a = 0;
+ $b = 1;
+
+ if (($a != 0) || ($b == 1) && ($b > $a) && ($b = 3) && ($a = 2))
+ {
+ 		echo "if works";
+ 		echo $a;
+ 		echo $b;
+ }
+?> 
+</body>
+</html>
